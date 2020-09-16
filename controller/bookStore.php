029495b9d294503c77767575302a7f1a375bc478
masal9pse <returnymgstokh@icloud.com>
2020-09-16 18:21:35 +0900

    テーブルにデータを追加した後に、遷移先を追記

--- controller/bookStore.php
+++ controller/bookStore.php
@@ -3,3 +3,6 @@ require('../dbconnect.php');
 
 $pdo->exec('insert into books set title="' . $_POST['title'] . '",description="' . $_POST['description'] . '",created_at=NOW()');
 echo '接続成功しました.';
+//header('Location: https://techacademy.jp/');
+?>
+<a href="../views/index.php">戻る</a>
