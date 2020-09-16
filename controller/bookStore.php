6a9d3484fa2adc2471ba64c50ba057bff5493e46
masal9pse <returnymgstokh@icloud.com>
2020-09-16 18:09:29 +0900

    tableにデータを送信できた。

--- controller/bookStore.php
+++ controller/bookStore.php
@@ -1,4 +1,5 @@
 <?php
 require('../dbconnect.php');
 
-$pdo->exec('insert into books set title=');
+$pdo->exec('insert into books set title="' . $_POST['title'] . '",created_at=NOW()');
+echo '接続成功しました.';
