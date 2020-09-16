224fe362538dabeb7f507373bd7b1c23d27cf51f
masal9pse <returnymgstokh@icloud.com>
2020-09-16 18:12:36 +0900

    投稿するデータのカラムを追加した。

--- controller/bookStore.php
+++ controller/bookStore.php
@@ -1,5 +1,5 @@
 <?php
 require('../dbconnect.php');
 
-$pdo->exec('insert into books set title="' . $_POST['title'] . '",created_at=NOW()');
+$pdo->exec('insert into books set title="' . $_POST['title'] . '",description="' . $_POST['description'] . '",created_at=NOW()');
 echo '接続成功しました.';
