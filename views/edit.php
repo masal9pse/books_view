3d5379c806a523d05829b2f421185ee3ea81c9f7
masal9pse <returnymgstokh@icloud.com>
2020-09-20 20:13:30 +0900

    booksテーブルのidに該当する値をフォームに表示

---
+++ views/edit.php
@@ -0,0 +1,33 @@
+<?php require('../dbconnect.php'); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>本の編集画面</title>
+</head>
+
+<body>
+ <?php
+ $books = $pdo->prepare('SELECT * FROM books where id = ?');
+ $books->execute(array(20));
+ $book = $books->fetch();
+ ?>
+ <form action="../controller/bookUpdate.php" method="post">
+  <div>
+   タイトル
+   <input type="text" name="title" value="<?php echo $book['title']; ?>">
+  </div>
+  <br>
+  <div>
+   内容
+   <textarea name="description" id="" cols="30" rows="10">
+   <?php echo $book['description']; ?>
+   </textarea>
+   <button type="submit">登録する</button>
+  </div>
+ </form>
+</body>
+
+</html>
