9d4494499d14fac051f3510257f1f1567e59338d
masal9pse <returnymgstokh@icloud.com>
2020-09-21 18:25:21 +0900

    dbにファイル名を登録

--- controller/bookStore.php
+++ controller/bookStore.php
@@ -1,6 +1,7 @@
 <?php
 require('../dbconnect.php');
 
-$pdo->exec('insert into books set title="' . $_POST['title'] . '",description="' . $_POST['description'] . '",created_at=NOW()');
+$picture = date('YmdHis') . $_FILES['picture']['name'];
+$pdo->exec('insert into books set title="' . $_POST['title'] . '",description="' . $_POST['description'] . '",picture="' . $picture . '",created_at=NOW()');
 //echo '接続成功しました.';
 header('Location: ../views/bookList.php');
