d1e4d413d71eb9d99a1adcb80c2b48f3c10dc455
masal9pse <returnymgstokh@icloud.com>
2020-09-22 00:17:30 +0900

    とりあえず簡易的な画像アップロードに成功

--- controller/bookStore.php
+++ controller/bookStore.php
@@ -3,5 +3,10 @@ require('../dbconnect.php');
 
 $picture = date('YmdHis') . $_FILES['picture']['name'];
 $pdo->exec('insert into books set title="' . $_POST['title'] . '",description="' . $_POST['description'] . '",picture="' . $picture . '",created_at=NOW()');
+//var_dump($_FILES['picture']['tmp_name']);
+move_uploaded_file($_FILES['picture']['tmp_name'], '../book_picture/' . $picture);
+$_SESSION['join'] = $_POST;
+$_SESSION['join']['picture'] = $picture;
 //echo '接続成功しました.';
 header('Location: ../views/bookList.php');
+exit();
