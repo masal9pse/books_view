977c4b2cd5893fef840457130079ce6e8151f47a
masal9pse <returnymgstokh@icloud.com>
2020-09-23 01:38:08 +0900

    dbに一括で複数データを投入したい。

--- seed/bookApiSeeder.php
+++ seed/bookApiSeeder.php
@@ -2,12 +2,11 @@
 
 try {
  $pdo = new PDO(
-  //'mysql:dbname=books_view;host=127.0.0.1;charset=utf8',
   'mysql:host=db;dbname=books_view_docker;charset=utf8',
   'root',
   'root',
  );
- $data = "https://www.googleapis.com/books/v1/volumes?q=湊かなえ";
+ $data = "https://www.googleapis.com/books/v1/volumes?q=宮沢賢治";
  $json = file_get_contents($data);
  $json_decode = json_decode($json);
  // jsonデータ内の『entry』部分を複数取得して、postsに格納
@@ -20,7 +19,7 @@ try {
  foreach ($books as $book) {
   // id
   //$id = $book->id;
-  $id = 2;
+  $id = 8;
   // タイトル
   $title = $book->volumeInfo->title;
   // 説明
@@ -31,10 +30,13 @@ try {
   $authors = implode(',', $book->volumeInfo->authors);
 
   $insert = array(':id' => $id++, ':title' => $title, ':author' => $authors, ':picture' => $thumbnail, ':description' => $description);
+  $insert_data->execute($insert);
+  //$insert_data->execute($book);
  }
- if ($insert_data->execute($insert)) {
-  echo "テーブルに保存完了です";
- }
+ echo "テーブルに保存完了です";
+ //if ($insert_data->execute($insert)) {
+ // echo "テーブルに保存完了です";
+ //}
 } catch (PDOException $e) {
  echo '接続できてません' . $e->getMessage();
 }
